95ac0ceab76f96310152e0deb06dd209d1df6399
Ansonseti <71368725+Ansonseti@users.noreply.github.com>
2021-09-03 19:16:16 +0700

    appeals.js ( please fix this more lol )

--- commands/appeals.js
+++ commands/appeals.js
@@ -2,6 +2,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js');
 const warnSchema = require('@schemas/warn-schema');
+const config = require('../config')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('appeal')
@@ -13,18 +14,20 @@ module.exports = {
         const punishmentId = interaction.options.getString('punishment_id');
         const appealReason = interaction.options.getString('reason');
 
-        const warningData = await warnSchema.findOne({userId: interaction.user.id}) //var is uh poisonous
+        const warningData = await warnSchema.findOne({userId: interaction.user.id})
         const warning = warningData.find(e => e.punishmentId == punishmentId)
         if(!warning)return interaction.reply({content: 'No such warning found.', ephemeral: true})
 
-        const channel = interaction.channel.type !== 'dm' ? await interaction.user.createDM() : interaction.channel;
+        const channel = interaction.channel.type !== 'dm' ? await interaction.user.createDM() : interaction.channel; //Not sure whats this, I'll leave it up to you ( idk how u plan to do dis )
+
+        interaciton.reply('Next, Please send your ad ( the one that you got warned for )') //PLEASE IMPROVE THIS, This is literally rly bad :(
 
         const filter = m => m.author.id == interaction.user.id;
 
         const collector = interaction.channel.createMessageCollector({ filter, time: 120000, max: 1 })
 
-        collector.on('collect', async msg => {
-            const appealsChannel = interaction.client.channels.cache.get(process.env.APPEALS_CHANNEL)
+        collector.on('collect', async interaction => {
+            const appealsChannel = interaction.client.channels.cache.get(config.appealsChannel)
 
             const embed = new Discord.MessageEmbed()
                 .setAuthor('Appeals')
@@ -33,10 +36,17 @@ module.exports = {
                     value: appealReason,
                 }, {
                     name: 'Advertisement',
-                    value: msg.content
+                    value: interaction.content
                 })
                 .setColor("RANDOM")
-                .setFooter('what this mate u configure')
+                .setFooter(`Requested by ${interaction.user.username}`)
+
+                appealsChannel.send({embeds: [embed]})
+                interaction.followUp('Appeal Submitted, Please wait for us to review your appeal')
+        })
+
+        collector.on('end', async interaction => {
+            interaction.reply('Times up, run the command again if you wanted to continue')
         })
     },
 };
